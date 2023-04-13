7974f2b30643ba846b01d9bb09180dc9ee41797c
Clark Winkelmann <clark.winkelmann@gmail.com>
2023-04-13 21:49:39 +0200

    Fix PHP Warning if setting contains NULL

--- src/ForumAttributes.php
+++ src/ForumAttributes.php
@@ -18,8 +18,10 @@ class ForumAttributes
         // Only include the conditions if we are in Frontend mode
         // The presence of the conditions will signal the frontend to use Frontend mode
         if ($this->settings->get('clarkwinkelmann-popular-discussion-badge.mode') !== 'scheduler') {
+            $conditions = json_decode((string)$this->settings->get('clarkwinkelmann-popular-discussion-badge.conditions'));
+
             return [
-                'popularDiscussionBadgeConditions' => json_decode($this->settings->get('clarkwinkelmann-popular-discussion-badge.conditions')),
+                'popularDiscussionBadgeConditions' => is_array($conditions) ? $conditions : [],
             ];
         }
 
